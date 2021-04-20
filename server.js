7206079cfe80c1ceb583244d2a4a2c64214691ec
Jesica Viola <jesica.viola@gmail.com>
2021-04-20 01:27:24 -0300

    register and login

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/profile', function (req, res) {
+  res.json({name: 'Juan Perez', email: 'juanperez@gmail.com'});
+})
+
+app.post('/register', function (req, res) {
+  var data = req.params;
+  console.log(data);
+  res.json({name: 'Juan Perez', email: 'juanperez@gmail.com'});
+})
+
+app.put('/login', function (req, res) {
+  var data = req.params;
+  console.log(data);
+  res.json({name: 'Juan Perez', email: 'juanperez@gmail.com'});
 })
 
 app.listen(port, function () {
